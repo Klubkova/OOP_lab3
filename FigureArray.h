13c7fccafee9e685bea592adb44f921d53aad42d
Klubkova <klubkova97@gmail.com>
2016-12-09 13:41:49 +0300

    monitor bug

---
+++ FigureArray.h
@@ -0,0 +1,30 @@
+// заголовки методов класса FigureArray
+#ifndef FIGUREARRAY_H
+#define FIGUREARRAY_H
+#include "Trapeze.h"
+#include "Pentagon.h"
+#include "Rhombus.h"
+#include "ArrayItem.h"
+#include <memory>
+const int size = 10; // размер массива
+class FigureArray
+{
+public:
+	FigureArray(); // конструктор
+	void Insert(std::shared_ptr<Pentagon> &pentagon, int index);
+	void Insert(std::shared_ptr<Rhombus> &rhombus, int index);
+	void Insert(std::shared_ptr<Trapeze> &trapeze, int index);
+	bool IsPentagon(int index);
+	bool IsRhombus(int index);
+	bool IsTrapeze(int index);
+	std::shared_ptr<Pentagon> GetPentagon(int index);
+	std::shared_ptr<Rhombus> GetRhombus(int index);
+	std::shared_ptr<Trapeze> GetTrapeze(int index);
+	void Delete(int index);
+	friend std::ostream& operator << (std::ostream &os, FigureArray &array);
+	virtual ~FigureArray(); // деструктор
+private:
+	ArrayItem a[size];
+};
+#endif
+
